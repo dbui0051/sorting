8cecd2a6cbd4b590729a3574f985487fe1e36d21
Josh White <whitejosh42@gmail.com>
2017-04-20 13:35:03 -0500

    changes to bubble

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,15 @@
-function bubbleSort(array) { // [8, 6, 10]
+function bubbleSort(array) {
+	var endLength = 1;
+
 	if(array.length === 0) return array;
+
 	do{
 		var looped = 0;
-		var endLength = 1;
-
 		for(let i = 0; i < array.length -endLength; i++){
 			if(array[i] > array[i+1]){
 				swapCount();
 				looped++;
-				var tempNum = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tempNum;
+				[array[i], array[i+1]] = [array[i+1], array[i]]
 			}
 		}
 		endLength++;
@@ -18,6 +17,8 @@ function bubbleSort(array) { // [8, 6, 10]
 
 	return array;
 }
+
+
 
 function swapCount(){
 	return;
